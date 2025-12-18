d81f3316231d7e64dc8b36ccf003ddf913e1ab15
Fedi Belakhel <fedi.belakhel@gmail.com>
2025-12-18 12:33:07 +0100

    fix: apply dedicated Prisma clients to auth-service and chat-service

--- backend/auth-service/src/prisma/prisma.service.ts
+++ backend/auth-service/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../node_modules/.prisma/client-auth';
 
 // 🗄️ Service Prisma - Gère la connexion à la base de données
 @Injectable()
